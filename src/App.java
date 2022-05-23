fb343f117157fc8a8f501ce4027a58bbcf1116be
DigitalRedPanda <osama-12341905@hotmail.com>
2022-05-24 01:14:09 +0300

    Uploading project

---
+++ src/App.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class App {
+    // TODO The plan is to create a command-line application that serves as a search
+    // engine for terms within my personal english dictionary along with support for
+    // different commands
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+    }
+}
