67ff8e7040269c0fe2f8f73ee22b4a699025624e
DigitalRedPanda <osama-12341905@hotmail.com>
2022-05-24 22:07:08 +0300

    Coverting project into a maven-dependent

--- src/main/java/com/digiunion/Run.java
+++ src/main/java/com/digiunion/Run.java
@@ -5,6 +5,6 @@ public class Run {
     // engine for terms within my personal english dictionary along with support for
     // different commands
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        var input = new Scanner(System.in);
     }
 }
