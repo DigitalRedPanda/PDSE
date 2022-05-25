ec7ce9cfddcc6d6702cea9aac1df5dca36db591f
DigitalRedPanda <osama-12341905@hotmail.com>
2022-05-25 23:07:11 +0300

    Changing the executor from ScheduledThreadPoolExecutor to Executors.newwFixxedThreadPool

--- src/main/java/com/digiunion/executor/Executor.java
+++ src/main/java/com/digiunion/executor/Executor.java
@@ -1,14 +1,14 @@
-package executor;
+package com.digiunion.executor;
 
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.Executors;
 
 import lombok.Getter;
 
 public abstract class Executor {
 
   @Getter
-  private static final ExecutorService executor = new ScheduledThreadPoolExecutor(
+  private static final ExecutorService executor = Executors.newFixedThreadPool(
       Runtime.getRuntime().availableProcessors());
 
 }
