74729a26176ef59f17a4c64534cb158ef3daed99
DigitalRedPanda <osama-12341905@hotmail.com>
2022-05-25 22:20:00 +0300

    Added an interface for DictionaryReader along with an implementation for its method

--- src/main/java/com/digiunion/reader/DictionaryReader.java
+++ src/main/java/com/digiunion/reader/DictionaryReader.java
@@ -1,7 +1,16 @@
 package com.digiunion.reader;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
-public class DictionaryReader {
- private final File source = new File("file: ./source/Dictionary.txt");
+public class DictionaryReader implements IDictionaryReader {
+
+ @Override
+ public void read(File file) throws IOException {
+  try (var reader = new BufferedReader(new FileReader("source\\Dictionary.txt"))) {
+
+  }
+ }
 }
---
+++ src/main/java/com/digiunion/reader/IDictionaryReader.java
@@ -0,0 +1,9 @@
+package com.digiunion.reader;
+
+import java.io.File;
+import java.io.IOException;
+
+public interface IDictionaryReader {
+
+ void read(File file) throws IOException;
+}
