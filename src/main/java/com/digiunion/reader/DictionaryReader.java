da9d274e6f3ed9b85b5ac95a152f1f7048b01312
DigitalRedPanda <osama-12341905@hotmail.com>
2022-05-25 18:33:35 +0300

    Defining the source file 'Dictionary.txt' object

--- src/main/java/com/digiunion/reader/DictionaryReader.java
+++ src/main/java/com/digiunion/reader/DictionaryReader.java
@@ -1,5 +1,8 @@
-package reader;
+package com.digiunion.reader;
+
+import java.io.File;
+import java.net.URL;
 
 public class DictionaryReader {
-
+ private final File source = new File("file: ./source/Dictionary.txt");
 }
