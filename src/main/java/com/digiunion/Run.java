4d66cbadcd82d18a85d45a405d55894aa4cccfbf
DigitalRedPanda <osama-12341905@hotmail.com>
2022-05-27 17:12:18 +0300

    Making the main method run on an independent thread asynchronously

--- src/main/java/com/digiunion/Run.java
+++ src/main/java/com/digiunion/Run.java
@@ -1,10 +1,24 @@
+package com.digiunion;
+
 import java.util.Scanner;
+import java.util.concurrent.CompletableFuture;
 
 public class Run {
     // TODO The plan is to create a command-line application that serves as a search
     // engine for terms within my personal english dictionary along with support for
     // different commands
     public static void main(String[] args) {
-        var input = new Scanner(System.in);
+        CompletableFuture<Void> future = new CompletableFuture<>();
+        future.runAsync(() -> {
+            try (var input = new Scanner(System.in)) {
+                System.out.println("Welcome to PDSE, type a word that you want to search");
+                var command = input.next();
+                System.out.println("Done!");
+                System.exit(0);
+            }
+        });
+
+        future.join();
+
     }
 }
